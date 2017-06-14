97ccb32dec2ee8bd74e6f3e3a5f03f9de82f4457
john sander <jrjackets112@gmail.com>
2017-06-14 14:02:05 -0400

    update view

--- models/user.js
+++ models/user.js
@@ -26,6 +26,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
+  },
 
    {
     // Creating a custom method for our User model. This will check if an unhashed password entered by
